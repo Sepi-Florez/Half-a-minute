82be589b4a14bf40560374a6d747de72f9424323
David V <david.vink@hotmail.com>
2017-10-29 14:44:09 +0100

    added settings menu and fixed menu anims

---
+++ Half-a-minute/Assets/Scripts/SettingHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingHelper : MonoBehaviour {
+    InputField counter;
+    Transform switcheroo;
+
+    public int myCounter;
+    public bool mySwitch;
+
+
+    public void Start() {
+        counter = transform.GetComponentInChildren<InputField>();
+    }
+    public void CounterIncrease(int i) {
+        myCounter += i;
+        counter.text = myCounter.ToString();
+    }
+    public void CounterChange(string i) {
+        myCounter = Convert.ToInt32(i);
+    }
+    public void Switcheroo() {
+        mySwitch = !mySwitch;
+    }
+}
