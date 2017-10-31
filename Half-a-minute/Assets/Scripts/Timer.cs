6ef0b675f8b9cc64df375476ed4ea2a4c1e8f1af
David V <david.vink@hotmail.com>
2017-10-31 20:17:24 +0100

    Everything works, will be doing some improvements

--- Half-a-minute/Assets/Scripts/Timer.cs
+++ Half-a-minute/Assets/Scripts/Timer.cs
@@ -1,16 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour {
+    public Text timerText;
+    public Image timerImag;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Text ButtonText;
+
+    bool timer;
+    float time;
+    float time2;
+    float timeExpended;
+
+    Coroutine timerLoop;
+    public void TimerToggle() {
+        if(!timer){
+            timer = true;
+            Reset();
+            timerLoop = StartCoroutine(Time());
+        }
+        else{
+            timer = false;
+            RefreshButton();
+        }
+
+    }
+    public IEnumerator Time() {
+        while (timer) {
+            yield return new WaitForSeconds(0.01f);
+            timerImag.fillAmount -= 0.01f;
+            time -= 0.01f;
+            timeExpended += 0.01f;
+            if (timeExpended >= 1) {
+                timerImag.fillAmount = 1.0f;
+                time2--;
+                timerText.text = time2.ToString();
+                timeExpended = 0;
+                if (time2 == 0) {
+                    timer = false;
+                    RefreshButton();
+                }
+            }
+        }
+    }
+    public void RefreshButton() {
+        if (timer) {
+            ButtonText.text = "Stop Timer";
+        }
+        else {
+            ButtonText.text = "Start Timer";
+        }
+    }
+    public void Reset() {
+        time = SettingsManager.thisManager.mySettings[1].myCounter;
+        time2 = time;
+        timerText.text = time.ToString();
+        RefreshButton();
+        timerImag.fillAmount = 1.0f;
+    }
 }
